be7b83950dc5a9751e098d238df33558206bd71b
Teriluna <catherinelozada7@gmail.com>
2021-03-03 09:52:46 -0500

    home page

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,7 @@
+class HomesController<ApplicationController
+  layout "home", only: [:index]
+
+  def index
+  end
+
+end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,8 +7,51 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.paths << Rails.root.join('fonts')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile +=
+  %w(
+      plugins/bootstrap/bootstrap.min.css
+      plugins/owl-carousel/owl.carousel.min.css
+      plugins/owl-carousel/owl.theme.default.min.css
+      plugins/magnific-popup/magnific-popup.min.css
+      plugins/sal/sal.min.css
+      theme.css
+      plugins/font-awesome/css/all.css
+      plugins/themify/themify-icons.css
+      plugins/simple-line-icons/css/simple-line-icons.css
+      chat.css
+
+      plugins/font-awesome/webfonts/fa-brands-400.eot
+      plugins/font-awesome/webfonts/fa-brands-400.svg
+      plugins/font-awesome/webfonts/fa-brands-400.ttf
+      plugins/font-awesome/webfonts/fa-brands-400.woff
+      plugins/font-awesome/webfonts/fa-brands-400.woff2
+
+      plugins/font-awesome/webfonts/fa-regular-400.eot
+      plugins/font-awesome/webfonts/fa-regular-400.svg
+      plugins/font-awesome/webfonts/fa-regular-400.ttf
+      plugins/font-awesome/webfonts/fa-regular-400.woff
+      plugins/font-awesome/webfonts/fa-regular-400.woff2
+
+      plugins/font-awesome/webfonts/fa-solid-900.eot
+      plugins/font-awesome/webfonts/fa-solid-900.svg
+      plugins/font-awesome/webfonts/fa-solid-900.ttf
+      plugins/font-awesome/webfonts/fa-solid-900.woff
+      plugins/font-awesome/webfonts/fa-solid-900.woff2
+
+      plugins/magnific-popup/*
+      plugins/owl-carousel/*
+      plugins/simple-line-icons/css/*
+      plugins/simple-line-icons/fonts/*
+      plugins/themify/**/*
+
+      jquery.min.js
+      images_loaded.js
+      appear.min.js
+      plugins.js
+      functions.js
+    )
