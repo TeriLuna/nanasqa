b181e44bc13e546222e3ecb30110a3a6f92972ad
Luis Rodriguez <luis.rodriguez@gorillalogic.com>
2021-03-08 11:20:27 -0500

    finish reservation form with iamges start date, end date adn message

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -9,4 +9,27 @@ class ReservationsController < ApplicationController
     @room = Room.find(params[:room_id])
     @reservation = Reservation.new
   end
+
+  def create
+    @room = Room.where(id: reservation_params[:room_id]).first
+    if @room.present?
+      @reservation = Reservation.
+        first_or_initialize(user_id: current_user.id, room_id: @room.id)
+
+      @reservation.attributes = reservation_params
+
+      if @reservation.save
+        redirect_to users_path
+      else
+        flash.now[:error] = @reservation.errors.full_messages.to_sentence
+        render :new
+      end
+    end
+  end
+
+  private
+  def reservation_params
+    params.require(:reservation).
+      permit(:room_id, :user_id, :start_date, :end_date, :message)
+  end
 end
