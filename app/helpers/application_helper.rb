f74d688c27767588e88b4531093edb69b35482c1
Luis Rodriguez <luis.rodriguez@gorillalogic.com>
2021-03-21 11:55:38 -0500

    fixing pay buttom to show value when is new reservation counting one person

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
 
-  def pay_buttom_text(reservation:, show_total: true, show_text: true)
+  def pay_buttom_text(reservation:, show_total: true, show_text: true, room: nil)
     if show_text == false
       if reservation.total.present? && params[:action] == "show"
         text = "$#{reservation.total}"
       else
-        per_person  = reservation&.room&.price_per_person || 0
+        per_person  = reservation&.room&.price_per_person || room.price_per_person
         quantity    = reservation.quantity
         total       = reservation.quantity * per_person
 
@@ -16,7 +16,7 @@ module ApplicationHelper
       if reservation.total.present? && params[:action] == "show"
         text = "Pay $#{reservation.total}"
       else
-        per_person  = reservation&.room&.price_per_person || 0
+        per_person  = reservation&.room&.price_per_person || room.price_per_person
         quantity    = reservation.quantity
         total       = reservation.quantity * per_person
 
