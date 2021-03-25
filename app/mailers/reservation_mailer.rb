547648f107f562614d0f110dbc366fb147e7584f
Luis Rodriguez <luis.rodriguez@gorillalogic.com>
2021-03-25 16:56:38 -0500

    add reservation mailer to app, and add staging env

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -2,6 +2,6 @@ class ReservationMailer < ApplicationMailer
   def reservation
     @user = params[:user]
     @reservation = params[:reservation]
-    mail(to: ENV['ADMIN_EMAIL'], subject: 'New Reservation')
+    mail(to: ENV['ADMIN_EMAIL'], subject: 'New Reservation - Nanasqa.com')
   end
 end
