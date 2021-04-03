f0097ed12724cc38efc554b8d96d38d752e88b44
Teriluna <catherinelozada7@gmail.com>
2021-04-03 18:49:49 -0500

    traduction to spanish

--- db/schema.rb
+++ db/schema.rb
@@ -94,8 +94,7 @@ ActiveRecord::Schema.define(version: 2021_04_02_203658) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "quantity", default: 1
-    t.decimal "total", default: "0.0"
-    t.jsonb "additional_info", default: {}
+    t.integer "total", default: 0
     t.integer "status", default: 0
   end
 
